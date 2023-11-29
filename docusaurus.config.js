0bdf164be7cefbd695738355c9a582e60ae2f4ec
Joshua Fernandes <joshua.fernandes@consensys.net>
2023-11-29 14:08:21 +1000

    fixing lint

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,5 @@
-const lightCodeTheme = require('prism-react-renderer').themes.github;
-const darkCodeTheme = require('prism-react-renderer').themes.dracula;
+const lightCodeTheme = require("prism-react-renderer").themes.github;
+const darkCodeTheme = require("prism-react-renderer").themes.dracula;
 
 const isDev = process.env.NODE_ENV === "development";
 const baseUrl = isDev ? "/" : "/";
