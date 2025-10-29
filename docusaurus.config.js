e065ba6389a7254b9390460d68be78f1bcfca545
Joshua Fernandes <joshua.fernandes@consensys.net>
2025-10-29 15:59:44 +1000

    update deps (#337)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,11 @@ const config = {
   url: "https://docs.web3signer.consensys.io",
   baseUrl,
   onBrokenLinks: "throw",
-  onBrokenMarkdownLinks: "throw",
+  markdown: {
+    hooks: {
+      onBrokenMarkdownLinks: "throw",
+    }
+  },
   favicon: "img/favicon.ico",
   trailingSlash: false,
 
